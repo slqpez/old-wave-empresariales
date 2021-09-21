a9d49b01b00f6f5c2b27ecd4e7b0c57a71ccf8db
sebasgp <sebastian.gallegop@udea.edu.co>
2021-09-21 15:57:57 -0500

    Feature footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import footerStyles from "./footer.module.css";
+import logoFooter from "../../assets/logos/logo-oldwave-footer.svg";
+import instagram from "../../assets/icons/icon-instagram.svg";
+import facebook from "../../assets/icons/icon-facebook.svg";
+import powered from "../../assets/logos/experimentality-vtex.svg"
+
+function Footer() {
+  return (
+    <footer className={footerStyles.footer}>
+      <section className={footerStyles.brandSection}>
+        <div className={footerStyles.footer__flex_item}>
+          <img src={logoFooter} alt="" />
+        </div>
+        <div className={footerStyles.footer__flex_item}>
+          <h2><span>old</span>wave</h2>
+          <ul>
+            <li>
+              <a href="">¿Quienes sómos?</a>
+            </li>
+            <li>
+              <a href="">Nuestras tiendas</a>
+            </li>
+          </ul>
+        </div>
+        <div className={footerStyles.footer__flex_item}>
+          <h2>Link de interés</h2>
+          <ul>
+            <li>
+              <a href="">Preguntas frecuentes</a>
+            </li>
+            <li>
+              <a href="">Terminos y condiciones</a>
+            </li>
+            <li>
+              <a href="">Politicas de devoluciones</a>
+            </li>
+          </ul>
+        </div>
+        <div className={footerStyles.footer__flex_item}>
+          <h2>Contácto</h2>
+          <ul>
+            <li>servicioalcliente@oldwave.com</li>
+            <li>Telefono: 316 1924</li>
+          </ul>
+          <h2 className={footerStyles.social_network}>Redes sociales</h2>
+          <img src={facebook} alt="Facebook logo" />
+          <img src={instagram} alt="Instagram logo" />
+        </div>
+      </section>
+      <section className={footerStyles.footerSection}>
+          <h3>oldwave© 2020 | Powered by:</h3>
+          <img src={powered} alt="" />
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
