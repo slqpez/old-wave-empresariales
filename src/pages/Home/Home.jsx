73602383ca0d1b9754be7205c1e4b2a007aac116
sebasgp <sebastian.gallegop@udea.edu.co>
2021-09-21 20:14:23 -0500

    Newsletter Styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Newsletter from '../../components/Newsletter/Newsletter'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Newsletter/>
     </div>
   )
 }
