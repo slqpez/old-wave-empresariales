3eaeefa4305c814d531a0adf3dd2876758362472
sebasgp <sebastian.gallegop@udea.edu.co>
2021-09-21 22:45:17 -0500

    Remove product card from dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Body from "./components/Body";
 import AppStyles from "./App.module.css";
-import ProductsCard from "./components/ProductsCard/ProductsCard";
 
 function App() {
   return (
     <div className={AppStyles.container}>
       <Router>
         <Body />
-        <ProductsCard/>
       </Router>
     </div>
   );
