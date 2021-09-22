2c023cfd8cac7849e9c647b9436acf4ea0e9d604
sebasgp <sebastian.gallegop@udea.edu.co>
2021-09-21 21:41:25 -0500

    Information Styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Information from '../../components/Information/Information'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Information/>
     </div>
   )
 }
