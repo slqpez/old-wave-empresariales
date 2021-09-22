5021436bd76bbf2e53a0852193fe8450e1f53e04
sebasgp <sebastian.gallegop@udea.edu.co>
2021-09-21 22:42:34 -0500

    Product Card Styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Body from "./components/Body";
 import AppStyles from "./App.module.css";
+import ProductsCard from "./components/ProductsCard/ProductsCard";
 
 function App() {
   return (
     <div className={AppStyles.container}>
       <Router>
         <Body />
+        <ProductsCard/>
       </Router>
     </div>
   );
---
+++ src/components/ProductsCard/ProductsCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import productsCardStyles from "./productsCard.module.css";
+import Prueba from "../../assets/desktop/producto1@2x.png";
+
+function ProductsCard() {
+  return (
+    <div className={productsCardStyles.ProductCard}>
+      <img src={Prueba} alt="" />
+      <div className={productsCardStyles.ProductCardInformation}>
+        <p className={productsCardStyles.ProductCardInformation_title}>El titulo del producto Lorem ipsum dolor</p>
+        <h4 className={productsCardStyles.ProductCardInformation_seller}>Samsung</h4>
+        <div className={productsCardStyles.Price_Container}>
+          <s>$1'500.000</s>
+          <p>$1'000.000</p>
+        </div>
+        <button className={productsCardStyles.ProductCard_btn}>Agregar al carrito</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductsCard;
