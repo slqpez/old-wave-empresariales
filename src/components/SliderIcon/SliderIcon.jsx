b383af574c9b64b7d30b736f298171445ffb74a7
joinerhs <herrerajoiner@gmail.com>
2021-09-23 20:30:43 -0500

    create files

---
+++ src/components/SliderIcon/SliderIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import sliderIconStyle from "./sliderIcon.module.css";
+
+function SliderIcon() {
+    return (
+        <div className = {sliderIconStyle.container}>
+            <h1>Aca va a ir el Slider</h1>
+        </div>
+    )
+}
+
+export default SliderIcon
