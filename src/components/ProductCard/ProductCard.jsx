588f81bbdf58d6e1cfe9f298503c861edf2c2047
slqpez <santiago.lopez16@udea.edu.co>
2021-10-15 17:13:37 -0500

    Styles detail product

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -60,6 +60,7 @@ function ProductsCard({
           JSON.stringify([...cartProducts, product])
         );
       }
+    
     }
   };
 
--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState, useContext } from "react";
 import productDetailStyle from "./productDetail.module.css";
-import {ProductsContext} from "../../context/productsContext"
+import { ProductsContext } from "../../context/productsContext";
 import { Link, useParams } from "react-router-dom";
 import {
   getProductByIDFlask,
   getProductByIDFast,
 } from "../../services/products";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+
 
 function ProductDetail() {
   const { search, product, id, seller } = useParams();
@@ -13,21 +16,17 @@ function ProductDetail() {
   const [{ cartProducts }, dispatch] = useContext(ProductsContext);
 
   useEffect(() => {
-   
     getProductByIDFast(id).then((data) => setDataProduct(data));
   }, []);
 
-
-
- 
   const handleAddToCart = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const image = dataProduct.pictures[0]
-    console.log(image)
-    const {name, price , id, brand} = dataProduct
+    const image = dataProduct.pictures[0];
+    console.log(image);
+    const { name, price, id, brand } = dataProduct;
     const product = { name, image, price, id, brand };
-    console.log(product)
+    console.log(product);
     if (cartProducts.length === 0) {
       dispatch({ type: "ADD_PRODUCT_TO_CART", payload: product });
       window.localStorage.setItem(
@@ -39,7 +38,7 @@ function ProductDetail() {
       const exists = cartProducts.find((p) => p.id === id);
 
       if (exists) {
-       /*  setQuantity((q) => q + 1); */
+        /*  setQuantity((q) => q + 1); */
       } else {
         const product = { name, image, price, id, brand };
         dispatch({ type: "ADD_PRODUCT_TO_CART", payload: product });
@@ -49,10 +48,9 @@ function ProductDetail() {
         );
       }
     }
-  }; 
+  };
 
-
-  if(Object.keys(dataProduct).length === 0) return <p>Cargando...</p>
+  if (Object.keys(dataProduct).length === 0) return <p>Cargando...</p>;
 
   return (
     <div className={productDetailStyle.detailContent}>
@@ -61,55 +59,72 @@ function ProductDetail() {
         <span> &gt;</span>
         <Link to={"/products/" + search}>{search}</Link>
         <span> &gt;</span>
-        <Link to={"/products/" + search + "/" + product + "/" + id}>
+        <Link
+          to={"/products/" + search + "/" + product + "/" + seller + "/" + id}
+        >
           {product}
         </Link>
 
-        
-
-       <div>
-          <div className={productDetailStyle.imagesContainer} data-cy="container-products">
-            {dataProduct ?.pictures.map((picture, i) => {
+        <div>
+          <div
+            className={productDetailStyle.imagesContainer}
+            data-cy="container-products"
+          >
+            {dataProduct?.pictures.map((picture, i) => {
               return (
                 <div key={i} className={productDetailStyle.imageContainer}>
                   <img src={picture} alt="Product image" />
                 </div>
               );
-            }) }
+            })}
+          </div>
+
+          <div className={productDetailStyle.containerAll}>
             <div className={productDetailStyle.mainImage}>
-              <img src={dataProduct?.pictures[0]} alt="Product image" data-cy="img-detail"/>
+              <img
+                src={dataProduct?.pictures[0]}
+                alt="Product image"
+                data-cy="img-detail"
+              />
+            </div>
+            <div className={productDetailStyle.infoProductContainer}>
+              <div className={productDetailStyle.infoMain}>
+                <h3>{dataProduct?.name}</h3>
+
+               
+                <p className={productDetailStyle.infoPrice}>
+                  {" "}
+                  {Intl.NumberFormat("es-CO", {
+                    style: "currency",
+                    currency: "COP",
+                    minimumFractionDigits: 0,
+                  }).format(dataProduct?.price)}
+                </p>
+               
+                <button
+                  className={productDetailStyle.Detail_btn}
+                  onClick={handleAddToCart}
+                  data-cy="btn-detail-add"
+                >
+                  Agregar al carrito
+                </button>
+              </div>
+              <div className={productDetailStyle.descriptionContainer}>
+                <h3>Descripción del producto</h3>
+                <p>Tipo de producto: {dataProduct?.description}</p>
+                <p>{dataProduct?.brand}</p>
+                <p>{dataProduct?.city?.name}</p>
+                <p>Calificación</p>
+                <h4>Vendedor:</h4>
+                <p>{dataProduct?.seller.name}</p>
+                <p><b>Calificación:</b></p>
+                <Rating name="read-only" value={dataProduct?.rating} readOnly />
+                
+               
+              </div>
             </div>
           </div>
-
-          <div className={productDetailStyle.infoProductContainer}>
-            <h3>{dataProduct?.name}</h3>
-
-            <p>{dataProduct?.brand}</p>
-            <p>{dataProduct?.city?.name}</p>
-            <p>
-              {" "}
-              {Intl.NumberFormat("es-CO", {
-                style: "currency",
-                currency: "COP",
-                minimumFractionDigits: 0,
-              }).format(dataProduct?.price)}
-            </p>
-
-            <div>
-              <h4>Vendedor</h4>
-              <p>{dataProduct?.seller.name}</p>
-            </div>
-            <button className={productDetailStyle.Detail_btn} onClick={handleAddToCart} data-cy="btn-detail-add">
-              Agregar al carrito
-            </button>
-          </div>
-          <hr className={productDetailStyle.DetailHr} />
-          <div className={productDetailStyle.descriptionContainer}>
-            <h3>Descripción del producto</h3>
-            <p>Tipo de producto: {dataProduct?.description}</p>
-            <p>Valoración: {dataProduct?.rating}</p>
-          </div>
-        </div> 
+        </div>
       </div>
     </div>
   );
