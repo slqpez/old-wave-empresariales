04c5a96d259b8bb311d3c8540df57df4605cea11
slqpez <santiago.lopez16@udea.edu.co>
2021-10-15 13:14:29 -0500

    Fix: beforeach-detailPage

--- cypress/integration/Pages/Detail.js
+++ cypress/integration/Pages/Detail.js
@@ -3,19 +3,19 @@
  export default class Detail{
 
     constructor(){
-      beforeEach(()=>{
-        cy.visit("http://localhost:3000/products/televisor/Televisor/undefined/1")
-      })
+      
     }
 
     renderPage(){
+      cy.visit("http://localhost:3000/products/televisor/Televisor/undefined/1")
         cy.get("[data-cy= container-products]")
         cy.get("[data-cy= img-detail]")
       } 
 
     addProductToCart(){
+      cy.visit("http://localhost:3000/products/televisor/Televisor/undefined/1")
         cy.get("[data-cy= btn-detail-add]").click()
         cy.get('[data-cy= cart-icon]').click()
-        cy.get('[data-cy= cart-list]').should('not.contain.text','no tienes productos agregados')
+        cy.get('[data-cy= cart-list]').should('not.contain.text','Aún no tienes productos agregados...😓')
     }
 }
